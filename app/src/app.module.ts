39e85994c0d9cac6e37f2e62ca549d916f73fefe
Kawano Kenichi <bppk28@gmail.com>
2021-09-05 10:42:20 +0000

    :art: Update app module

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TodoModule } from "./todo/todo.module";
+import { UtilsModule } from "./utils/utils.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [TodoModule, UtilsModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
