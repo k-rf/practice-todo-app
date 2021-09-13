9af46871a1366a28cf0ab1a5b4ff3c373479b9a1
Kawano Kenichi <bppk28@gmail.com>
2021-09-13 04:09:45 +0000

    :wrench: Update configurations

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import reactJsx from "vite-react-jsx";
+import { readDirectory } from "./script/read-directory";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [reactRefresh()],
-    server: { host: "0.0.0.0", port: 8080 },
+export default defineConfig(async () => {
+    const alias = (await readDirectory("src", { type: "directory" })).map(
+        (e) => ({
+            find: `${e}`,
+            replacement: `${__dirname}/src/${e}`,
+        }),
+    );
+
+    console.log(alias);
+
+    return {
+        plugins: [reactRefresh(), reactJsx()],
+        server: { host: "0.0.0.0", port: 8080 },
+        resolve: {
+            alias,
+        },
+        optimizeDeps: { exclude: ["node_modules"] },
+    };
 });
